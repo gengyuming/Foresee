c86b3f0348d6d9f8649b72494bf276654cfdb1ce
gengyuming <576796616@qq.com>
2021-10-21 22:12:49 +0800

    update

--- Calculator/TicketCalculator.py
+++ Calculator/TicketCalculator.py
@@ -19,19 +19,32 @@ class LottoCalculator:
     def get_results(result_type='source'):
         sql = 'SELECT * FROM ticket_history'
         rows = mysql_conn.execute_sql(sql)
-        results = {}
+        results = []
         if result_type == 'sort':
             for row in rows:
-                results[row.get('draw_number')] = row.get('draw_sort_result')
+                results.append(row)
         if result_type == 'source':
             for row in rows:
                 if row.get('draw_source_result'):
-                    results[row.get('draw_number')] = row.get('draw_source_result')
+                    results.append(row)
 
         return results
 
+    def calculate_date_factor(self, draw_number, data_list):
+        """
+        计算日期因素
+        :param draw_number: 期号
+        :param data_list: 历史数据
+        :return:
+        """
+        for data in data_list:
+            current_date = data['draw_time'].replace('-', '')
+
+
 
 lc = LottoCalculator()
+res = lc.get_results()
+
 
 
 
